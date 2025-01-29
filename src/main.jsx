0fe8397bf84d6e8bbd0e5b76ce8d89130cff571e
nienying <nienying.h@houseofdev.tech>
2025-01-29 18:58:20 +0700

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,15 @@
-// import { StrictMode } from 'react'
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 import { ToastContainer } from "react-toastify";
+import { store } from "./store.js";
 import { Provider } from "react-redux";
-import { store } from "./store";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
   <Provider store={store}>
     <App />
-    <ToastContainer position="top-center"/>
+    <ToastContainer position="top-center" />
   </Provider>
-  // </StrictMode>,
 );
